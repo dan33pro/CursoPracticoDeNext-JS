9868e64c69ee4ad0873c14fbb2946a28e80030c7
daniel cespedes <daniel@DanielC-PC>
2022-10-19 06:10:02 -0500

    Configuración inical del proyecto

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true, // ;
+    singleQuote: true, // '
+    printWidth: 200, // Cracteres en pantalla
+    tabWindth: 2,
+    useTabs: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+}
