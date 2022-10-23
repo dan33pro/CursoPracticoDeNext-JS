9cc385f43cab00e69e8ee45dc775a4a39a44fca5
daniel cespedes <daniel@DanielC-PC>
2022-10-23 13:24:41 -0500

    Update Etiquetas de Google Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,13 @@ class MyDocument extends Document{
             <Html lang="en">
                 <Head>
                     <title>React:Shop</title>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-C689L541VP"></script>
+                    <script dangerouslySetInnerHTML={{
+                        __html: `window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-C689L541VP');`
+                    }} />
                 </Head>
                 <body>
                     <Main />
