26e413236316c20a15def4fe41f62d7103aa81c7
daniel cespedes <daniel@DanielC-PC>
2022-10-23 12:20:04 -0500

    FIX basname

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
 
   return (
     <AppContext.Provider value={initialState}>
-      <BrowserRouter basename='/CursoPractico-React-JS'>
+      <BrowserRouter basename='/'>
       {/* <BrowserRouter> */}
         <Layout>
           <Routes>
