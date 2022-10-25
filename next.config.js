e98f786d70851e979c01df495ef9b150d9e9531c
daniel cespedes <daniel@DanielC-PC>
2022-10-25 09:07:42 -0500

    Script next export

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['placeimg.com', 'www.google.com', 'api.lorem.space', 'www.pngkey.com', 'images.unsplash.com'],
+    unoptimized: true,
   },
 };
 
